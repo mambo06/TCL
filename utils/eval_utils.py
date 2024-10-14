0a0229391c04ed2ac6467ba6d0a9a8fda826c454
mambo06 <mambo06@users.noreply.github.com>
2024-10-14 23:30:12 +1000

    Update eval_utils.py

--- utils/eval_utils.py
+++ utils/eval_utils.py
@@ -159,26 +159,27 @@ def linear_model_eval(config, z_train, y_train, suffix , z_test, y_test,z_val, y
             # Fit model to the data
             # clf = KNeighborsClassifier(n_neighbors=c)
 
-            clf = xgb.XGBClassifier()
-            # param_grid = {"max_depth":    [8],
-            #       "n_estimators": [ 1000],
-            #       "learning_rate": [0.015]}
-            search = GridSearchCV(clf, param_grid, cv=2,verbose=2, n_jobs=-1).fit(z_train, y_train)
-            print("The best hyperparameters are ",search.best_params_)
+            # clf = xgb.XGBClassifier()
+            # # param_grid = {"max_depth":    [8],
+            # #       "n_estimators": [ 1000],
+            # #       "learning_rate": [0.015]}
+            # search = GridSearchCV(clf, param_grid, cv=2,verbose=2, n_jobs=-1).fit(z_train, y_train)
+            # print("The best hyperparameters are ",search.best_params_)
 
-            clf = xgb.XGBClassifier(learning_rate = search.best_params_["learning_rate"],
-                           n_estimators  = search.best_params_["n_estimators"],
-                           max_depth     = search.best_params_["max_depth"],
-                           eval_metric='logloss')
-            # clf = xgb.XGBClassifier(learning_rate = param_grid["learning_rate"][-1],
-            #                n_estimators  = param_grid["n_estimators"][-1],
-            #                max_depth     = param_grid["max_depth"][-1],
-            #                eval_metric='mlogloss',
-            #                # early_stopping_rounds = 10,
-            #                verbosity = 2)
+            # clf = xgb.XGBClassifier(learning_rate = search.best_params_["learning_rate"],
+            #                n_estimators  = search.best_params_["n_estimators"],
+            #                max_depth     = search.best_params_["max_depth"],
+            #                eval_metric='logloss')
+            clf = xgb.XGBClassifier(learning_rate = param_grid["learning_rate"][-1],
+                           n_estimators  = param_grid["n_estimators"][-1],
+                           max_depth     = param_grid["max_depth"][-1],
+                           # eval_metric='mlogloss',
+                           # early_stopping_rounds = 10,
+                           colsample_bytree=0.5,
+                           verbosity=0)
 
 
-            # clf.fit(z_train, y_train,  eval_set=[(z_val, y_val)])
+            clf.fit(z_train, y_train,  eval_set=[(z_val, y_val)])
             clf.fit(z_train, y_train)
         
             y_hat_train = clf.predict(z_train)
